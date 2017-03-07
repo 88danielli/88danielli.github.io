66c036d3c5244f9af5775f1a85fba765a4043aa7
Daniel Li <88danielli@gmail.com>
2017-03-06 22:47:29 -0800

    changes to ui.bootstrap, fixed accordion

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -4,11 +4,12 @@ app.controller('projectsController', function($scope) {
   console.log('Projects Controller Loaded');
 
   $scope.oneAtATime = true;
-  
+
   $scope.status = {
     isCustomHeaderOpen: false,
-    isFirstOpen: true,
-    isFirstDisabled: false
+    isFirstOpen: false,
+    isFirstDisabled: false,
+    isSecondOpen: false,
   };
 
   })
